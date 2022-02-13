3c269c6a3b78592678f988def4fdbe6f1f724040
fine <dlgmltjr0925@gmail.com>
2022-02-13 22:20:58 +0900

    네비게이션

---
+++ tutorial/DetailController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailController.swift
+//  tutorial
+//
+//  Created by 이희석 on 2022/03/05.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
