d80f468c17f3913b8fac665a54150507a788d901
fine <dlgmltjr0925@gmail.com>
2022-02-16 14:22:13 +0900

    웹뷰

---
+++ tutorial/WebViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WebViewController.swift
+//  tutorial
+//
+//  Created by 이희석 on 2022/03/05.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: ViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) { // unwrapp
+            let urlRequest = URLRequest(url: url)
+            webView.load(urlRequest)
+        }
+    }
+}
